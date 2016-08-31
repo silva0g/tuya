183edcaa9fc442fe4be2261442cea86d8c4dd0a1
silva0g <silvanazinha@gmail.com>
2016-08-31 21:18:02 +0200

    Added custom actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+  	member do
+  		put :change
+  	end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
